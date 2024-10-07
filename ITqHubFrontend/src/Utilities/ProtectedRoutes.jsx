9f2b3e1b2b2e195f8c2fe8632c7d39b730a7233f
Venkat Gudipati <98337647+gvr9094@users.noreply.github.com>
2024-10-07 15:38:11 +0530

    Initial commit

--- ITqHubFrontend/src/Utilities/ProtectedRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from 'react-router-dom';
-
-export default function ProtectedRoutes() {
-    const isAuthenticated = localStorage.getItem("authToken") !== null;
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-}
